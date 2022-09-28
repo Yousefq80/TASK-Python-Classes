44b234c351d5be0529da1cc7ea15e22416b7e7a6
Yousefq80 <ysalharby@gmail.com>
2022-09-28 21:08:12 +0300

    not final answer

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+from unicodedata import name
+
+
 class Wallet:
-    def __init__(self, money):
-        pass
+    def __init__(self, money=0):
+        self.money=money
 
-    def credit(amount):
-        pass
+    def credit(self,amount):
+        self.amount=amount
+        self.money=self.amount+self.money
+        print(f"The total amount is:{self.money}")
 
-    def debit(amount):
-        pass
-
+    def debit(self,amount):
+        self.amount=amount
+        self.money=self.amount-self.money
+        print(f"The total amount is:{self.money}")
 
 wallet = Wallet(6)
 wallet = Wallet()  # This should default money inside the wallet to 0
@@ -16,16 +22,23 @@ print(wallet)
 
 class Person:
     # Implement the code here
-    pass
+    def __init__(self,name,location,wallet,moveto):
+                self.name=name
+                self.location=location
+                self.walllet=wallet
+                self.moveto=moveto
 
 
 person = Person("Moh", 5, 50)
 
 
-class Vendor:
+class Vendor(person):
     # implement Vendor!
-    pass
-
+    def __init__(self,name,location,range=5,price=1,sellto=0):
+         super().__init__(name,location)
+         self.range=range
+         self.price=price
+         self.sellto=sellto
 
 vendor = Vendor("Abdallah", 3, 6)
 
